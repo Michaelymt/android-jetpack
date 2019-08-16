7778d2cf418bf704ac99ed4595041625b1d3e9b2
YURI MONTAÑEZ <mmontanezt@gmail.com>
2019-08-16 00:14:40 -0500

    first commit

---
+++ app/src/main/java/com/ymt/MainActivity.java
@@ -0,0 +1,17 @@
+package com.ymt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Tratando de desaparecer el Toolbar
+        getSupportActionBar().hide();
+    }
+}
